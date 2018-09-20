c98c580a0410e93907b7749aee8ef9687755ace0
Mi Kyung Han <mhan@calbaptist.edu>
2018-09-20 10:54:35 -0700

    adding an important instruction for the required class names

--- src/BabyNames.java
+++ src/BabyNames.java
@@ -20,6 +20,9 @@ public class BabyNames {
     // Test with both "names.txt" and "names2.txt" (Before submission, change back to "names.txt")
     private static final String NAME_FILENAME = "names.txt";
 
+    // Put other class constants here
+    // You must use the exact name given in the spec for the required class constants
+
     public static void main(String[] args) {
         //Remove this comment and below line and implement your code
         System.out.println("Import Success! Now delete this code and implement your own!");
