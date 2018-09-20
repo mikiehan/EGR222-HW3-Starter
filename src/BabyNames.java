6dc77aa6d35a89f5ba44fe33a89f3b118ebeb796
Mi Kyung Han <mhan@calbaptist.edu>
2018-09-20 10:11:56 -0700

    Adding more support files

--- src/BabyNames.java
+++ src/BabyNames.java
@@ -1,6 +1,7 @@
 /**
  * Created by YOUR_NAME on DATE
  * Put comments in your code
+ * Should NOT use array
  * Follow Style Guide provided in BB
  * Follow freedom of information and gilligans island rule
  * Discussed with: XXX
@@ -9,6 +10,15 @@
 import java.awt.*;
 
 public class BabyNames {
+
+    // Filename class constant for meaning
+    // Do NOT change the name of this constant
+    private static final String MEANING_FILENAME = "meanings.txt";
+
+    // Filename class constant for name
+    // Do NOT change the name of this constant
+    // Test with both "names.txt" and "names2.txt" (Before submission, change back to "names.txt")
+    private static final String NAME_FILENAME = "names.txt";
 
     public static void main(String[] args) {
         //Remove this comment and below line and implement your code
