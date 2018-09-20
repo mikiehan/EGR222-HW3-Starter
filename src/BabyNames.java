ff5d2813d6a7532f2ce113576ddd5b732d76b62e
Mi Kyung Han <mhan@calbaptist.edu>
2018-09-20 01:16:50 -0700

    initializing repo with starter code for HW3

---
+++ src/BabyNames.java
@@ -0,0 +1,17 @@
+/**
+ * Created by YOUR_NAME on DATE
+ * You shouldn't use any if-else statements
+ * Put comments in your code
+ * Follow freedom of information and gilligans island rule
+ * Discussed with: XXX
+ **/
+
+import java.awt.*;
+
+public class BabyNames {
+
+    public static void main(String[] args) {
+        //Remove this comment and below line and implement your code
+        System.out.println("Import Success! Now delete this code and implement your own!");
+    }
+}
