3addebc9ae793263a556dfb0f130c0e51aa43704
Mi Kyung Han <mhan@example.com>
2020-09-10 15:38:10 -0700

    adding constants

--- src/BabyNames.java
+++ src/BabyNames.java
@@ -10,6 +10,11 @@
 import java.awt.*;
 
 public class BabyNames {
+    // Required constants declared here
+    // Do NOT change the name of these constants but can change the value
+    private static final Integer STARTING_YEAR = 0; // change the value according to spec
+    private static final Integer DECADE_WIDTH = 0; // change the value according to spec
+    private static final Integer LEGEND_HEIGHT = 0; // change the value according to spec
 
     // Filename class constant for meaning
     // Do NOT change the name of this constant
@@ -20,9 +25,13 @@ public class BabyNames {
     // Test with both "names.txt" and "names2.txt" (Before submission, change back to "names.txt")
     private static final String NAME_FILENAME = "names.txt";
 
-    // Put other class constants here
-    // You must use the exact name given in the spec for the required class constants
+    //prompt msg class constants
+    private static final String MESSAGE_PREFIX = "This program allows you to search through the\n" +
+            "data from the Social Security Administration\n" +
+            "to see how popular a particular name has been\n" +
+            "since ";
 
+    // YOU ARE NOT ALLOWED TO ADD ANY OTHER CONSTANTS THAN ABOVE
     public static void main(String[] args) {
         //Remove this comment and below line and implement your code
         System.out.println("Import Success! Now delete this code and implement your own!");
