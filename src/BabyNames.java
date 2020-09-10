c950a81c65bfc6bbafdf3b2ef6f8518a5a9a156f
Mi Kyung Han <mhan@example.com>
2020-09-10 16:03:06 -0700

    adding more constants

--- src/BabyNames.java
+++ src/BabyNames.java
@@ -10,26 +10,28 @@
 import java.awt.*;
 
 public class BabyNames {
-    // Required constants declared here
-    // Do NOT change the name of these constants but can change the value
-    private static final Integer STARTING_YEAR = 0; // change the value according to spec
-    private static final Integer DECADE_WIDTH = 0; // change the value according to spec
-    private static final Integer LEGEND_HEIGHT = 0; // change the value according to spec
 
-    // Filename class constant for meaning
-    // Do NOT change the name of this constant
-    private static final String MEANING_FILENAME = "meanings.txt";
+    //class constants for open area in graphics (Do NOT change)
+    private static final Integer OPEN_AREA_WIDTH = 780;
+    private static final Integer OPEN_AREA_HEIGHT = 500;
 
-    // Filename class constant for name
-    // Do NOT change the name of this constant
-    // Test with both "names.txt" and "names2.txt" (Before submission, change back to "names.txt")
-    private static final String NAME_FILENAME = "names.txt";
-
-    //prompt msg class constants
+    //prompt msg class constants (Do NOT change)
     private static final String MESSAGE_PREFIX = "This program allows you to search through the\n" +
             "data from the Social Security Administration\n" +
             "to see how popular a particular name has been\n" +
             "since ";
+
+    // class constant for meaning file (Do NOT change)
+    private static final String MEANING_FILENAME = "meanings.txt";
+
+    // class constant for name file (Change the value only. Do NOT change the names of the constant)
+    // Test with both "names.txt" and "names2.txt" (Before submission, change back to "names.txt")
+    private static final String NAME_FILENAME = "names.txt";
+
+    // Other class constants (Change the value only. Do NOT change the names of the constants)
+    private static final Integer STARTING_YEAR = 0; // change the value according to spec
+    private static final Integer DECADE_WIDTH = 0; // change the value according to spec
+    private static final Integer LEGEND_HEIGHT = 0; // change the value according to spec
 
     // YOU ARE NOT ALLOWED TO ADD ANY OTHER CONSTANTS THAN ABOVE
     public static void main(String[] args) {
